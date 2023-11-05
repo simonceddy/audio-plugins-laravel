766b5a795feed1f190a3c5ceef7cab63709b0247
Simon Eddy <simon@simoneddy.com.au>
2023-11-06 04:18:28 +1100

    controller boilerplate

---
+++ app/Http/Controllers/Plugins/ViewPlugin.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Plugins;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ViewPlugin extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
